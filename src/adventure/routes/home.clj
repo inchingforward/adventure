2d5f63a03f9898068aaa489246b20762a4945028
mj <inchingforward@gmail.com>
2013-08-07 12:44:06 -0500

    Converted to Luminus

---
+++ src/adventure/routes/home.clj
@@ -0,0 +1,15 @@
+(ns adventure.routes.home
+  (:use compojure.core)
+  (:require [adventure.views.layout :as layout]
+            [adventure.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
