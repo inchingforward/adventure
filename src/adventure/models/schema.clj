6fdf5ca9ed7b99e153a1b8a50694dd4173b85b50
inchingforward <inchingforward@gmail.com>
2013-08-11 09:39:02 -0500

    Added default db settings.

--- src/adventure/models/schema.clj
+++ src/adventure/models/schema.clj
@@ -5,5 +5,5 @@
 (def db-spec
   {:subprotocol "postgresql"
    :subname "//localhost/adventure"
-   :user (util/env-var "ADV_DB_USER")
-   :password (util/env-var "ADV_DB_PASS")})
+   :user (util/env-var "ADV_DB_USER" "adventure")
+   :password (util/env-var "ADV_DB_PASS" "adventure")})
