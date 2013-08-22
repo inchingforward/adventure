61806ab21667c2c45a6a3e026df2da44fadb323e
mj <inchingforward@gmail.com>
2013-08-22 13:12:29 -0500

    Added markdown support to adventure details and scenes.

--- src/adventure/views/layout.clj
+++ src/adventure/views/layout.clj
@@ -1,6 +1,12 @@
 (ns adventure.views.layout
   (:use noir.request)
-  (:require [selmer.parser :as parser]))
+  (:require [selmer.parser :as parser]
+            [selmer.filters :as filters]
+            [markdown.core :as md]))
+
+(filters/add-filter! :markdown
+                     (fn [s]
+                       [:safe (md/md-to-html-string s)]))
 
 (def template-path "adventure/views/templates/")
 
